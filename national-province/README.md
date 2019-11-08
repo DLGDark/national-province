9711301cb7ab9eef7fa1fe90392c43a852d83378
dlg <1399477141.qq.com>
2019-11-08 17:42:37 +0800

    update

--- national-province/README.md
+++ national-province/README.md
@@ -1,6 +1,6 @@
 
 **调用方法名：**
- - getProvince
+ - getProvince(Promise异步调用，内部代码已将执行结果以JSON格式写入文件，并返回)
 
 **参数：**
 
@@ -8,6 +8,23 @@
 
  **返回值：**
 
- - 无
+ - 所有省份(json格式)
 
 **结果：** 获取到国家省级地区信息并写入json文件
+
+**代码中使用方式:**
+```javascript
+// 文件头部require方式引入
+const province=require('national-province');
+
+// 代码中使用
+province.getProvince()
+        .then(res => {
+          // 这里写拿到数据后的自己的一些操作代码
+          // 因为内部是异步调用方式，这里用then来接受结果，结果为查找到的所有省数据，数据格式为json格式
+          console.log(res);
+        })
+        .catch(err => { 
+          console.log(err);
+        });
+```
--- national-province/lib/getProvince.js
+++ national-province/lib/getProvince.js
@@ -8,51 +8,55 @@ const iconv = require('iconv-lite');
 const fs = require('fs');
 
 // 获取当前年份值
-var year = new Date().getFullYear() -1;
+var year = new Date().getFullYear() - 1;
 // 目的网址
-var url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/'+year+'/index.html';
+var url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/' + year + '/index.html';
 
-function getProvince(){
-  http.get(url,(res) => {
-        let chunkBuffers=[];
+function getProvince() {
+  const resultPromise = new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      let chunkBuffers = [];
 
-        res.on('data', (chunk) => {
-            chunkBuffers.push(chunk);
-        });
+      res.on('data', (chunk) => {
+        chunkBuffers.push(chunk);
+      });
 
-        res.on('end', () => {
-            try {
-                // 对字节进行处理
-                let buffer=Buffer.concat(chunkBuffers);
-                // 对window环境下的返回字符编码为gb2312编码格式的字符内容进行处理
-                let html = iconv.decode(buffer, 'gb2312');
+      res.on('end', () => {
+        try {
+          // 对字节进行处理
+          let buffer = Buffer.concat(chunkBuffers);
+          // 对window环境下的返回字符编码为gb2312编码格式的字符内容进行处理
+          let html = iconv.decode(buffer, 'gb2312');
 
-                let $provinceCheerio = cheerio.load(html);
-                let $province = $provinceCheerio('.provincetr');
-                let provinceObj = [];
+          let $provinceCheerio = cheerio.load(html);
+          let $province = $provinceCheerio('.provincetr');
+          let provinceObj = [];
 
-                let $td = '';
-                $province.each(function (i, item) {
-                  $td = $provinceCheerio(item).find('td');
-                  $td.each(function (i, item) {
-                    let resObj = {
-                      provinceCode: $provinceCheerio(item).find('a').attr('href').split('.')[0],
-                      provinceName: $provinceCheerio(item).find('a').text()
-                    }
-                    provinceObj.push(resObj);
-                  });
-                });
-                // 将数据写入文件
-                fs.writeFile(year+'国家省区信息.json', JSON.stringify(provinceObj), (err) => {
-                  if (err) throw err;
-                  console.log('file saved');
-                });
-            } catch (e) {
-                console.error(e.message);
-            }
-        }).on('error', (e) => {
-            console.error(`出现错误: ${e.message}`);
-        });
+          let $td = '';
+          $province.each(function (i, item) {
+            $td = $provinceCheerio(item).find('td');
+            $td.each(function (i, item) {
+              let resObj = {
+                provinceCode: $provinceCheerio(item).find('a').attr('href').split('.')[0],
+                provinceName: $provinceCheerio(item).find('a').text()
+              }
+              provinceObj.push(resObj);
+            });
+          });
+          // 将数据写入文件
+          fs.writeFile(year + '国家省区信息.json', JSON.stringify(provinceObj), (err) => {
+            if (err) throw err;
+            console.log('file saved');
+            resolve(JSON.stringify(provinceObj));
+          });
+        } catch (e) {
+          console.error(e.message);
+        }
+      }).on('error', (e) => {
+        console.error(`出现错误: ${e.message}`);
+      });
     });
+  });
+  return resultPromise;
 }
-module.exports={ getProvince };
+module.exports = { getProvince };
